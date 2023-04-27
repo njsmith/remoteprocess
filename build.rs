d20dc9e8b2b83f113f15e6a8b40d11102f7b6f4f
Nathaniel J. Smith <njs@anthropic.com>
2023-04-27 14:29:23 -0700

    Statically link libunwind

--- build.rs
+++ build.rs
@@ -20,19 +20,35 @@ fn main() {
                     println!("cargo:rustc-link-search=native=/usr/local/lib");
 
                     let out_dir = env::var("OUT_DIR").unwrap();
-                    std::fs::copy(format!("/usr/local/musl/{}/lib/libunwind.a", target), format!("{}/libunwind-remoteprocess.a", out_dir)).unwrap();
-                    std::fs::copy(format!("/usr/local/musl/{}/lib/libunwind-ptrace.a", target), format!("{}/libunwind-ptrace.a", out_dir)).unwrap();
-                    std::fs::copy(format!("/usr/local/musl/{}/lib/libunwind-{}.a", target, target_arch), format!("{}/libunwind-{}.a", out_dir, target_arch)).unwrap();
-                    std::fs::copy(format!("/usr/local/musl/{}/lib/libz.a", target), format!("{}/libz.a", out_dir)).unwrap();
+                    std::fs::copy(
+                        format!("/usr/local/musl/{}/lib/libunwind.a", target),
+                        format!("{}/libunwind-remoteprocess.a", out_dir),
+                    )
+                    .unwrap();
+                    std::fs::copy(
+                        format!("/usr/local/musl/{}/lib/libunwind-ptrace.a", target),
+                        format!("{}/libunwind-ptrace.a", out_dir),
+                    )
+                    .unwrap();
+                    std::fs::copy(
+                        format!("/usr/local/musl/{}/lib/libunwind-{}.a", target, target_arch),
+                        format!("{}/libunwind-{}.a", out_dir, target_arch),
+                    )
+                    .unwrap();
+                    std::fs::copy(
+                        format!("/usr/local/musl/{}/lib/libz.a", target),
+                        format!("{}/libz.a", out_dir),
+                    )
+                    .unwrap();
                     println!("cargo:rustc-link-search=native={}", out_dir);
                     println!("cargo:rustc-link-lib=static=unwind-remoteprocess");
                     println!("cargo:rustc-link-lib=static=unwind-ptrace");
                     println!("cargo:rustc-link-lib=static=unwind-{}", target_arch);
                     println!("cargo:rustc-link-lib=static=z");
                 } else {
-                    println!("cargo:rustc-link-lib=dylib=unwind");
-                    println!("cargo:rustc-link-lib=dylib=unwind-ptrace");
-                    println!("cargo:rustc-link-lib=dylib=unwind-{}", target_arch);
+                    println!("cargo:rustc-link-lib=static=unwind");
+                    println!("cargo:rustc-link-lib=static=unwind-ptrace");
+                    println!("cargo:rustc-link-lib=static=unwind-{}", target_arch);
                 }
             }
         }
